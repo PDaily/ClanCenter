6850d76b74d63945c437ae3ad5b46a0a76eaf41e
pDaily <pat@pdaily.me>
2015-06-18 23:44:22 -0400

    Fixed allowed params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,7 +64,7 @@ class UsersController < ApplicationController
   end
 
   private
-  
+
   # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
@@ -73,6 +73,6 @@ class UsersController < ApplicationController
   # Never trust parameters from the scary internet,
   # only allow the white list through.
   def user_params
-    params.require(:user).permit(:user_name, :avatar, :time_zone, :xbox_name, :steam_name, :twitch_name)
+    params.require(:user).permit(:email, :password, :user_name, :avatar, :time_zone, :xbox_name, :steam_name, :twitch_name)
   end
 end
