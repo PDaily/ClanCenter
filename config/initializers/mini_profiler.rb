db30ac0769dc793649429e2831270f8ae9bab8bb
Pat <pat.irwin4@gmail.com>
2015-07-08 00:04:27 +0000

    Fix rack-mini-profiler initializer for dev & test

--- config/initializers/mini_profiler.rb
+++ config/initializers/mini_profiler.rb
@@ -1,2 +1,4 @@
-require 'rack-mini-profiler'
-Rack::MiniProfiler.config.skip_paths = ['/admin']
+if Rails.env.test? || Rails.env.development?
+    require 'rack-mini-profiler'
+    Rack::MiniProfiler.config.skip_paths = ['/admin']
+end
