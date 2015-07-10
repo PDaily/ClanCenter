645344150c95a5ee5b336b386f081b60be3d2e43
Pat <pat.irwin4@gmail.com>
2015-07-10 00:22:39 +0000

    Fix mod permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,19 +9,19 @@ class Ability
       can :access, :rails_admin   # grant access to rails_admin
 			can :dashboard
 		elsif user.has_role?(:mod)
-			can :manage, [ GameSession, Game, GameMode ]
 			can :read, :all
 			can :access, :rails_admin   # grant access to rails_admin
-			can :new, [Game, GameMode, GameSession]           # included in :create
 			can :export, [Game, GameMode, GameSession]
-			can :destroy, [Game, GameMode, GameSession]       # for BulkDelete
+			can :destroy, [Game, GameMode, SiteNews]       # for BulkDelete
+			can [:edit, :create], [Game, GameMode, GameSession, SiteNews]
 		end
       can :read, [ GameSession, Game, User ]
-			can [:destroy, :edit], GameSession, :creator_id => user.id # manage owned gamesessions
+			can :edit, GameSession, :creator_id => user.id # manage owned gamesessions
 			can :create, GameSession # create gamesession
 			can [:update_games, :sort_by_game, :end_game], GameSession
 			can :join_game, GameSession # join gamesessions
 			can :leave_game, GameSession # leave gamesessions
 			can [:edit, :update, :destroy], User, :id => user.id # manage own profile
+			can :read, SiteNews
   end 
 end
